4d255266d69f6ce43d47eb822ffc04f7878f200e
Kartik Kopargaonkar <103638908+kartik-dev18@users.noreply.github.com>
2022-04-14 23:08:46 +0530

    Update streamlit_app.py
    Show list of fruits selected.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,12 @@ my_fruit_lst = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dab
 my_fruit_lst = my_fruit_lst.set_index('Fruit')
 
 
-streamlit.multiselect("Pick some fruits :", list(my_fruit_lst.index),['Banana','Apple'])
+fruits_selected = streamlit.multiselect("Pick some fruits :", list(my_fruit_lst.index),['Banana','Apple'])
+fruits_to_show = my_fruit_lst.loc[fruits_selected]
 
-streamlit.dataframe(my_fruit_lst)
+#streamlit.dataframe(my_fruit_lst)
+streamlit.dataframe(fruits_to_show)
+
 
 
 
